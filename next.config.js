54498ef6e0d8403be620126620a0a2cbe59d9fd3
Agustín Quetto <agustinquetto@gmail.com>
2019-04-09 10:44:04 -0300

    canary

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-      target: 'serverless'
-    }
-
-    return config
-  }
+  target: 'serverless'
 }
